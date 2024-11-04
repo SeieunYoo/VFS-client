a9e6605fb10131b7245220ad5ae1d960524119c7
SeieunYoo <isho48239@gmail.com>
2024-11-04 18:13:41 +0900

    feat: 버튼 width full 로 조정

--- app/details/[id]/page.tsx
+++ app/details/[id]/page.tsx
@@ -91,8 +91,8 @@ const DetailPage = () => {
           </div>
         </div>
         <div>
-          <div className="flex px-4 py-3">
-            <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-12 px-5 flex-1 bg-black text-white text-base font-bold leading-normal tracking-[0.015em]">
+          <div className="flex py-3">
+            <button className="flex w-full lg:max-w-[412px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-12 px-5 flex-1 bg-black text-white text-base font-bold leading-normal tracking-[0.015em]">
               <span className="truncate">Try On Live</span>
               <CameraIcon />
             </button>
